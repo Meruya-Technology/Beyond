7e938af0a391683c698165b0cccc427d23207817
Dwi Kurnianto Mulyadien <dwikurnianto.mulyadien@gmail.com>
2023-01-20 13:28:39 +0700

    Update util

--- beyond/lib/src/utils/encrypt_util.dart
+++ beyond/lib/src/utils/encrypt_util.dart
@@ -1,19 +1,13 @@
-import 'package:beyond/beyond.dart';
 import 'package:encrypt/encrypt.dart';
-import 'package:get_it/get_it.dart';
 
 class EncryptUtil {
-  static final _env = GetIt.instance<Env>();
-
   static String encrypt(
     String text, {
     Algorithm? algorithm,
+    required String secretKey,
   }) {
     final encrypter = Encrypter(
-      algorithm ??
-          AES(
-            Key.fromBase64(_env.secretKey),
-          ),
+      algorithm ?? AES(Key.fromBase64(secretKey)),
     );
     return encrypter.encrypt(text, iv: IV.fromLength(16)).base64;
   }
