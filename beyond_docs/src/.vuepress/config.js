e13ff43d10f31adcc48d96281bfb7506090ce20f
Dwi Kurnianto Mulyadien <dwikurnianto.mulyadien@gmail.com>
2023-01-20 07:25:47 +0700

    Update docs project

---
+++ beyond_docs/src/.vuepress/config.js
@@ -0,0 +1,75 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Beyond',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#0046FA' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/site.webmanifest"}],
+    ['link', { rel: "shortcut icon", href: "/favicon.ico"}],
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ beyond_docs/src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: /b-512-light.png
+tagline: Documentation for beyond dart framework
+actionText: Quick Start →
+actionLink: /guide/
+features:
+- title: Easy to use
+  details: Write simple backend easier with beyond
+- title: Resource sharing
+  details: Share model between front end and backend application
+- title: Documentation
+  details: A simple text could be easy
+footer: Made by Meruya Technology with ❤️
+---
