9b1eb33158471e2bf4fa6eafed5dbcb5a6f4a833
Dwi Kurnianto Mulyadien <dwikurnianto.mulyadien@gmail.com>
2023-01-31 17:25:45 +0700

    Update commandrunner name

--- beyond_cli/bin/main.dart
+++ beyond_cli/bin/main.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
-import 'package:beyond_cli/command_runner.dart';
+import 'package:beyond_cli/beyond_cli.dart';
 
 Future<int> main(List<String> args) async {
-  final exitCode = await beyondCommandRunner(args);
+  final exitCode = await beyondCli(args);
   exit(exitCode);
 }
--- beyond_cli/lib/beyond_cli.dart
+++ beyond_cli/lib/beyond_cli.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:beyond_cli/src/core/beyond_command.dart';
 
-Future<int> beyondCommandRunner(List<String> args) async {
+Future<int> beyondCli(List<String> args) async {
   int exitCode = await BeyondCommand.dispatchCommand(args);
   await Future.wait<void>([stdout.close(), stderr.close()]);
   return exitCode;
